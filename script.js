0a56c28388e472bda8c4719fb314a7bb7c093dff
Alexandredlima81 <31992131+alexandredlima81@users.noreply.github.com>
2024-09-15 20:16:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,7 @@ const translations = {
         navContact: "Contact",
         aboutTitle: "About Me",
         aboutDescription: `Hello, everyone!<br><br>
-        My name is Alexandre, a professional in Information and Communication Technology, always looking for challenges to evolve in knowledge! 🚀<br><br>
-        Who am I? Well, I have always worked in the Infrastructure area.<br><br>
-        In recent years, I have dived deep into AWS technologies "❤️" and DevOps practices, aiming to become a Jedi of Technology 😅, both in the On-Premise world, Datacenters, and Public Cloud ☁️.<br><br>
-        What do I do? I am always navigating between processes, people, and technological solutions, with the mission to ensure that each solution is in sync with the goals and needs of the organizations where I work.<br><br>
-        My goal? To continue on the path of professional and personal development, seeking new technological adventures and opportunities to grow, develop, and collaborate more and more 💡.<br><br>
-        And as part of this journey, I come through this page here on Github.io to share topics related to our day-to-day needs, accepting suggestions and help so that together we can strengthen the community of shared knowledge.`,
+            My name is Alexandre, a professional in Information and Communication Technology...`,
         projectsTitle: "My Projects",
         project1Title: "Project 1",
         project1Description: "Description of project 1.",
@@ -30,12 +25,7 @@ const translations = {
         navContact: "Contato",
         aboutTitle: "Sobre Mim",
         aboutDescription: `Olá, pessoal!<br><br>
-            Meu nome é Alexandre, profissional da Tecnologia da Informação e Comunicação que está em busca constante de desafios para evoluir em conhecimento! 🚀<br><br>
-            Quem sou eu? Bem, atuando desde sempre na área de Infraestrutura.<br><br>
-            Nos últimos anos, mergulhei de cabeça em tecnologias da AWS "❤️" e práticas DevOps, em busca de me tornar mais um Jedi da Tecnologia 😅, tanto no mundo On-Premise, Datacenters quanto em Cloud Pública ☁️.<br><br>
-            O que faço? Estou sempre navegando entre processos, pessoas e soluções tecnológicas, com a missão de garantir que cada solução esteja em sintonia com os objetivos e necessidades das organizações onde atuo.<br><br>
-            Meu objetivo? Continuar a trilhar o caminho do desenvolvimento profissional e pessoal, buscando novas aventuras tecnológicas e oportunidades para crescer, desenvolver e colaborar cada vez mais 💡.<br><br>
-            E como parte desta caminhada, venho através desta página aqui no Github.io, compartilhar tópicos relacionados às nossas necessidades do dia a dia, aceitando sugestões e ajuda para que juntos possamos fortalecer a comunidade do conhecimento compartilhado.`,
+            Meu nome é Alexandre, profissional da Tecnologia da Informação e Comunicação...`,
         projectsTitle: "Meus Projetos",
         project1Title: "Projeto 1",
         project1Description: "Descrição do projeto 1.",
