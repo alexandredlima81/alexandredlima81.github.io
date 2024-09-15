17abce3fc92d4c45ce9b3a55c52e6ba8c2dd226e
Alexandredlima81 <31992131+alexandredlima81@users.noreply.github.com>
2024-09-15 20:10:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const translations = {
         navProjects: "Projects",
         navContact: "Contact",
         aboutTitle: "About Me",
-        aboutDescription: "Professional in the field of IT Operations and Communication. Focused on management and support for people, processes, and technological solutions related to infrastructure (On Premise / Cloud). My goal is to continue professional and personal development.",
+        aboutDescription: `Hello, everyone!<br><br>
+        My name is Alexandre, a professional in Information and Communication Technology, always looking for challenges to evolve in knowledge! 🚀<br><br>
+        Who am I? Well, I have always worked in the Infrastructure area.<br><br>
+        In recent years, I have dived deep into AWS technologies "❤️" and DevOps practices, aiming to become a Jedi of Technology 😅, both in the On-Premise world, Datacenters, and Public Cloud ☁️.<br><br>
+        What do I do? I am always navigating between processes, people, and technological solutions, with the mission to ensure that each solution is in sync with the goals and needs of the organizations where I work.<br><br>
+        My goal? To continue on the path of professional and personal development, seeking new technological adventures and opportunities to grow, develop, and collaborate more and more 💡.<br><br>
+        And as part of this journey, I come through this page here on Github.io to share topics related to our day-to-day needs, accepting suggestions and help so that together we can strengthen the community of shared knowledge.`,
         projectsTitle: "My Projects",
         project1Title: "Project 1",
         project1Description: "Description of project 1.",
@@ -49,15 +55,4 @@ function changeLanguage(lang) {
     document.getElementById('nav-projects').textContent = translations[lang].navProjects;
     document.getElementById('nav-contact').textContent = translations[lang].navContact;
     document.getElementById('about-title').textContent = translations[lang].aboutTitle;
-    document.getElementById('about-description').innerHTML = translations[lang].aboutDescription;
-    document.getElementById('projects-title').textContent = translations[lang].projectsTitle;
-    document.getElementById('project1-title').textContent = translations[lang].project1Title;
-    document.getElementById('project1-description').textContent = translations[lang].project1Description;
-    document.getElementById('project2-title').textContent = translations[lang].project2Title;
-    document.getElementById('project2-description').textContent = translations[lang].project2Description;
-    document.getElementById('project3-title').textContent = translations[lang].project3Title;
-    document.getElementById('project3-description').textContent = translations[lang].project3Description;
-    document.getElementById('contact-title').textContent = translations[lang].contactTitle;
-    document.getElementById('contact-description').innerHTML = translations[lang].contactDescription;
-    document.getElementById('footer-text').textContent = translations[lang].footerText;
-}
+    document
